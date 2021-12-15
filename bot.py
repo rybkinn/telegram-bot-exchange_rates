5a441138077f2d52e84ff85d8e5f91a4013bd4e5
Nikita Rybkin <4rybkin@gmail.com>
2021-12-15 21:29:32 +0300

    template aiogram bot

--- bot.py
+++ bot.py
@@ -1,4 +1,34 @@
 # Python telegram bot "Exchange rates".
 # Powered by Nikita Rybkin.
+import logging
 
-print("Hi world!")
+from aiogram import Bot, Dispatcher, executor, types
+
+import config
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+
+# Initialize bot and dispatcher
+bot = Bot(token=config.API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    # start bot
+    executor.start_polling(dp, skip_updates=True)
